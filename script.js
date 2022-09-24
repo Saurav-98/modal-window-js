e20f46e086f047ae6f833cafb5d15229516e7f23
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-09-24 23:56:07 +0530

    Complete 1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modalEl = document.querySelector('.modal');
+const overlayEl = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const showBtns = document.querySelectorAll('.show-modal');
+
+const hideModal = () => {
+  modalEl.classList.add('hidden');
+  overlayEl.classList.add('hidden');
+};
+
+const showModal = () => {
+  modalEl.classList.remove('hidden');
+  overlayEl.classList.remove('hidden');
+};
+
+for (let i = 0; i < showBtns.length; i++) {
+  showBtns[i].addEventListener('click', showModal);
+}
+
+closeBtn.addEventListener('click', hideModal);
+
+overlayEl.addEventListener('click', hideModal);
